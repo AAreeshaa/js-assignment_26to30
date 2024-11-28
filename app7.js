6c6423968a214e7befdf48eb2347ba45d4977e74
Areesha Ali <171791528+AAreeshaa@users.noreply.github.com>
2024-11-28 21:37:33 +0500

    Add files via upload

---
+++ app7.js
@@ -0,0 +1,81 @@
+// //chapter 26 to 30
+// //Question no 1
+// var number = parseInt(prompt("Give me a positive integer "));
+// var roundedoff = Math.round(number);
+// var floorVal = Math.floor(number);
+// var ceilVal = Math.ceil(number);
+
+// document.write = ("number:"+ number + "<br/>")
+// document.write = ("round off value:" + roundedoff + "<br/>")
+// document.write = ("floor value:" + floorVal + "<br/>")
+// document.write= ("ceil value" + ceilVal + "<br/>")
+
+
+// //Question no 2
+// var number = parseInt(prompt("Give me a negative integer "));
+// var roundedoff = Math.round(number);
+// var floorVal = Math.floor(number);
+// var ceilVal = Math.ceil(number);
+
+// document.write = ("number:"+ number + "<br/>")
+// document.write = ("round off value:" + roundedoff + "<br/>")
+// document.write = ("floor value:" + floorVal + "<br/>")
+// document.write= ("ceil value" + ceilVal + "<br/>")
+
+
+// //Question no 3
+// let numbers = parseInt(prompt("Enter a number"));
+// let absolute = Math.abs(number);
+// document.write = ("the absolute number of "+ number + "is" + absolute );
+
+
+// //Question no 4
+// let diceVal = Math.floor(Math.random() * 6) + 1 ;
+// document.write = ("random dice value is "+ diceVal);
+
+
+// //Question no 5
+// let coinToss = Math.floor(Math.random() *2 );
+// let coin = coinToss === 0 ? "Heads" : "tails";
+// document.write = ("random coin value:" + coin);
+
+
+// //Question no 6
+// let num = parseInt(prompt("Enter a number between 1 to 100"))
+// let ans = Math.floor(Math.random() * 100 ) + 1 ;
+// document.write = ("Random number between 1 to 100:" + ans)
+
+// //Question no 7
+// let weight = ("Enter your weight in Kilograms")
+// let weightVal = parseFloat(weight)
+// document.write = ("the weight of user is "+weightVal+ "kilograms")
+
+
+// //Question no 8
+// let secretnum = parseInt(prompt("Guess a secret number"))
+// let randomNum = Math.floor(Math.random()*10) + 1
+// if ( randomNum === secretnum) {
+//     document.write ("Congratulation! you guess the right number"+ randomNum)
+// } else{
+//      document.write = ("Opps! Try again"+ randomNum + "is the correct number")
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
